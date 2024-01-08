237f1409c7835866fa0cbfa029312d770e0af24e
Wellington M. Alves <wmalves@outlook.com>
2024-01-08 17:25:49 -0300

    feat: configure dbcontext

---
+++ LibraTrack.Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿namespace LibraTrack.Infra.Data;
+
+public sealed class ApplicationDbContext(DbContextOptions options) : DbContext(options), IUnitOfWork
+{
+    private static readonly JsonSerializerSettings _jsonSerializerSettings = new()
+    {
+        TypeNameHandling = TypeNameHandling.All
+    };
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        
+        base.OnModelCreating(modelBuilder);
+    }
+    
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var result = await base.SaveChangesAsync(cancellationToken);
+
+            return result;
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            throw new ConcurrencyException("Concurrency exception occurred.", ex);
+        }
+    }
+}
