cc49e32f69dad6c89cacbbc5e5e21e20caa91162
Wellington M. Alves <wmalves@outlook.com>
2024-01-08 17:47:31 -0300

    feat: ajust api dependency injection and migrations

---
+++ src/LibraTrack.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraTrack.Api.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        
+        dbContext.Database.Migrate();
+    }
+}
---
+++ src/LibraTrack.Api/GlobalUsings.cs
@@ -0,0 +1,6 @@
+// Global using directives
+
+global using LibraTrack.Api.Extensions;
+global using LibraTrack.Infra;
+global using LibraTrack.Infra.Data;
+global using Microsoft.EntityFrameworkCore;
