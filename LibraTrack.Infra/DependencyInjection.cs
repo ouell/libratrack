d19677675c93f6684fc9d083d655177702c1b01c
Wellington M. Alves <wmalves@outlook.com>
2024-01-08 17:42:50 -0300

    feat: create dependecy injection for infrastructure.

---
+++ LibraTrack.Infra/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using LibraTrack.Infra.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibraTrack.Infra;
+
+public static class DependencyInjection
+{
+
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+                                                       IConfiguration configuration)
+    {
+        AddPersistence(services, configuration);
+
+        return services;
+    }
+
+    private static void AddPersistence(IServiceCollection services,
+                                       IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database") ??
+                               throw new ArgumentException(null, nameof(configuration));
+
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<ILoanRepository, LoanRepository>();
+        services.AddScoped<IBookRepository, BookRepository>();
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
+    }
+}
